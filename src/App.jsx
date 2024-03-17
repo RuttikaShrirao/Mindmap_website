184aea573c00354022ea00119e7450565b388e05
RuttikaShrirao <ruttikashrirao@gmail.com>
2024-03-17 23:51:45 +0530

    structure is ready for minmap web

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import ReactFlow from 'reactflow';
+ 
+import 'reactflow/dist/style.css';
+ 
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: 'Research', position: { x: -300, y: 90 }, data: { label: 'Research' } },
+  { id: 'Planning', position: { x: -100, y: 90 }, data: { label: 'Planning' } },
+  { id: 'Desiging', position: { x: 100, y: 90 }, data: { label: 'Desiging' } },
+  { id: 'Manufacturing', position: { x: 300, y: 90 }, data: { label: 'Manufacturing' } },
+  { id: 'Sales/Marketing', position: { x: 500, y: 90 }, data: { label: 'Sales/Marketing' } },
 
-function App() {
-  const [count, setCount] = useState(0)
+  { id: 'External', position: { x: -400, y: 200 }, data: { label: 'External' } },
+  { id: 'Internal', position: { x: -200, y: 200 }, data: { label: 'Internal' } },
 
+];
+const initialEdges = [{ id: 'e1-1', source: '1', target: 'Research' },
+{ id: 'e1-2', source: '1', target: 'Planning' },
+{ id: 'e1-3', source: '1', target: 'Desiging' },
+{ id: 'e1-4', source: '1', target: 'Manufacturing' },
+{ id: 'e1-5', source: '1', target: 'Sales/Marketing' },
+
+{ id: 'e2-1', source: 'Research', target: 'External' },
+{ id: 'e2-2', source: 'Research', target: 'Internal' },
+];
+ 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    </div>
+  );
 }
-
-export default App
