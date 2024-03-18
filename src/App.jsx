2f09697489200d27ea07c97b4ce64d8e2b572c9e
RuttikaShrirao <ruttikashrirao@gmail.com>
2024-03-18 22:52:54 +0530

    style node into structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactFlow from 'reactflow';
- 
+// import ReactFlow from 'reactflow';
+import ReactFlow, { Controls, MiniMap } from 'react-flow-renderer';
 import 'reactflow/dist/style.css';
  
 const initialNodes = [
@@ -25,10 +25,30 @@ const initialEdges = [{ id: 'e1-1', source: '1', target: 'Research' },
 { id: 'e2-2', source: 'Research', target: 'Internal' },
 ];
  
+const nodeStyle = {
+  width: 100, // Set the width of the node
+  height: 50, // Set the height of the node
+  background: '#0099ff',
+  color: '#fff',
+  borderRadius: '8px',
+  padding: '8px',
+  textAlign: 'center',
+};
 export default function App() {
+  const onLoad = (reactFlowInstance) => {
+    reactFlowInstance.fitView();
+  };
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    <div style={{ height: '100vh',width:"100vw",display:'flex',justifyContent:'center', border: '1px solid red' }}>
+    {/* <div style={{position:'relative'}}> */}
+      <ReactFlow onLoad={onLoad}
+      style={{ height: '100%', width: '100%' }}
+      snapToGrid={true}
+        snapGrid={[15, 15]}
+       nodes={initialNodes} 
+       edges={initialEdges} />
+      {/* </div> */}
+      {/* {ini  */}
     </div>
   );
 }
